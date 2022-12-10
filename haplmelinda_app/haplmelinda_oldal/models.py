c8898f8432828c0009e637f91d4e5f046b775027
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-12-10 23:09:07 +0100

    kép megjelenítés

--- haplmelinda_app/haplmelinda_oldal/models.py
+++ haplmelinda_app/haplmelinda_oldal/models.py
@@ -1,19 +1,11 @@
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    # kep = models.ImageField()
+class Kep(models.Model):
+    cim = models.CharField(max_length=200)
+    leiras = models.CharField(max_length=500)
+    keszites_datuma = models.DateTimeField('date published')
+    kep = models.ImageField(upload_to='upload/')
 
     def __str__(self):
-        return self.question_text
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.choice_text
+        return self.cim
--- haplmelinda_app/haplmelinda_oldal/views.py
+++ haplmelinda_app/haplmelinda_oldal/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from .models import Kep
 
 def index(request):
-    return render(request, 'kepek.html', {})
+    kepek = Kep.objects.all()
+    return render(request, 'kepek.html', {"kepek_tomb": kepek})
+
+from django.views.generic import DetailView
+
+class EmpImageDisplay(DetailView):
+    model = Kep
+    template_name = 'emp_image_display.html'
+    context_object_name = 'emp'
