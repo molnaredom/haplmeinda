fea4eed53ca47ddaacad4c05b68f589027d9c20a
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-12-12 04:49:48 +0100

    uj modell felallitas

--- haplmelinda_app/haplmelinda_oldal/models.py
+++ haplmelinda_app/haplmelinda_oldal/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class Kep(models.Model):
     cim = models.CharField(max_length=200)
     leiras = models.CharField(max_length=500)
+    ar = models.IntegerField()
+    meret = models.CharField(max_length=200)
+    tema = models.CharField(max_length=200)
+    technika = models.CharField(max_length=200)
+    eladva = models.BooleanField()
     keszites_datuma = models.DateTimeField('date published')
     kep = models.ImageField(upload_to='upload/')
 
