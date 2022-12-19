0825afe35a3af580ace7c932a0f01c6fb4bb7e24
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-12-19 18:13:49 +0100

    insert into automatikus kép insertáló --> csv bol adatbazisba tesz

--- haplmelinda_app/haplmelinda_oldal/models.py
+++ haplmelinda_app/haplmelinda_oldal/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Kep(models.Model):
     sorszam = models.IntegerField()
     cim = models.CharField(max_length=200)
+    leiras = models.CharField(max_length=500)
+    ar = models.IntegerField()
     belso_meret = models.CharField(max_length=200)
     kulso_meret = models.CharField(max_length=200)
     ar = models.IntegerField()
@@ -11,7 +13,9 @@ class Kep(models.Model):
     technika = models.CharField(max_length=200)
     leiras = models.CharField(max_length=500)
     eladva = models.BooleanField()
+    # keszites_datuma = models.DateTimeField('date published')
     kep = models.ImageField(upload_to='upload/')
 
     def __str__(self):
         return self.cim
+
