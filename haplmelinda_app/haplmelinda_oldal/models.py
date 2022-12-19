92497ba31f05d19d33e8d8ac58e50cd395d0fa11
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-12-19 16:45:58 +0100

    modell kiterjesztése , ennek megfelelően változtatva az adatbázis szövegek

--- haplmelinda_app/haplmelinda_oldal/models.py
+++ haplmelinda_app/haplmelinda_oldal/models.py
@@ -4,14 +4,13 @@ from django.db import models
 class Kep(models.Model):
     sorszam = models.IntegerField()
     cim = models.CharField(max_length=200)
-    leiras = models.CharField(max_length=500)
-    ar = models.IntegerField()
     belso_meret = models.CharField(max_length=200)
     kulso_meret = models.CharField(max_length=200)
+    ar = models.IntegerField()
     tema = models.CharField(max_length=200)
     technika = models.CharField(max_length=200)
+    leiras = models.CharField(max_length=500)
     eladva = models.BooleanField()
-    # keszites_datuma = models.DateTimeField('date published')
     kep = models.ImageField(upload_to='upload/')
 
     def __str__(self):
