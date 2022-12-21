6f91cae71592810f444e63d699cb280637ec169a
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-12-21 10:28:42 +0100

    Oldal gyorsítási probálkozás, https modulo helyett, mar modulo a viewsben, de ez nem oldotta meg

--- haplmelinda_app/haplmelinda_oldal/views.py
+++ haplmelinda_app/haplmelinda_oldal/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
 from .models import Kep
 
-from django import template
-
-register = template.Library()
-
-@register.filter
-def modulo(num, val):
-    return num % val
-
 
 def kep_reszletek(request, id):
     konkret_kep = Kep.objects.get(id=id)
     return render(request, 'kep_reszletek.html', {"kep" : konkret_kep})
 
-
+from django.db.models import F
 def album(request):
-    kepek = Kep.objects.all().order_by('?')
-    return render(request, 'kepek.html', {"kepek_tomb": kepek})
+    kepek = Kep.objects.all()  #.order_by('?')
+
+    oszlop1 = kepek.annotate(idmod3=F('id') % 3).filter(idmod3=0)
+    oszlop2 = kepek.annotate(idmod3=F('id') % 3).filter(idmod3=1)
+    oszlop3 = kepek.annotate(idmod3=F('id') % 3).filter(idmod3=2)
+
+    return render(request, 'kepek.html',
+                  {"kepek_tomb": kepek, "o1":oszlop1, "o2":oszlop2, "o3":oszlop3, })
 
 def rolam(request):
     return render(request, 'rolam.html', {})
