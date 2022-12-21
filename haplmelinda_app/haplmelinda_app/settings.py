1d3a72f37976d8efe9a84790520a9fde7116cbbb
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2022-12-21 05:47:33 +0100

    Egymás mellé csúszó képek, nincs űr a hoszabb vagy rövidebb képek közt, de lassabb a betöltés

--- haplmelinda_app/haplmelinda_app/settings.py
+++ haplmelinda_app/haplmelinda_app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'haplmelinda_oldal'
+    'haplmelinda_oldal',
+    'haplmelinda_oldal.templatetags'
 ]
 
 MIDDLEWARE = [
--- haplmelinda_app/haplmelinda_oldal/views.py
+++ haplmelinda_app/haplmelinda_oldal/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Kep
 
+from django import template
+
+register = template.Library()
+
+@register.filter
+def modulo(num, val):
+    return num % val
+
+
 def kep_reszletek(request, id):
     konkret_kep = Kep.objects.get(id=id)
     return render(request, 'kep_reszletek.html', {"kep" : konkret_kep})
