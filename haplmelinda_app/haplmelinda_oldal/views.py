16a26989927b6ace9c9d63bc690febd6e0529004
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2023-03-07 18:31:40 +0100

    DEMO szűrő

--- haplmelinda_app/haplmelinda_oldal/views.py
+++ haplmelinda_app/haplmelinda_oldal/views.py
@@ -9,13 +9,22 @@ def kep_reszletek(request, id):
 from django.db.models import F
 def album(request):
     kepek = Kep.objects.all().order_by('?')
+    print(request.POST)
+    if "tema" in request.POST:
+        print("téma léesz a téma", request.POST)
+        tema_neve = request.POST["tema"]
+        print(tema_neve)
+        eredemeny =Kep.objects.all().filter(tema=tema_neve)
+        print(eredemeny)
+    else:
+        print("nem sikerult")
 
     oszlop1 = kepek.annotate(idmod3=F('id') % 3).filter(idmod3=0)
     oszlop2 = kepek.annotate(idmod3=F('id') % 3).filter(idmod3=1)
     oszlop3 = kepek.annotate(idmod3=F('id') % 3).filter(idmod3=2)
 
     return render(request, 'kepek.html',
-                  {"kepek_tomb": kepek, "o1":oszlop1, "o2":oszlop2, "o3":oszlop3, })
+                  {"kepek_tomb": kepek, "o1": oszlop1, "o2": oszlop2, "o3": oszlop3, })
 
 def rolam(request):
     return render(request, 'rolam.html', {})
