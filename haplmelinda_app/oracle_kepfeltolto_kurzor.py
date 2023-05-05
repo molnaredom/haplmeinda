d7ce2108aa7ab884927fd4a0c83f33a04db9d94e
molnar-laptop-fujitsu <molnaradam8466@gmail.com>
2023-05-05 09:41:00 +0200

    Tema es technika idk és nevekek tábláinak feltöltése + kurzor hozzájuk

--- haplmelinda_app/oracle_kepfeltolto_kurzor.py
+++ haplmelinda_app/oracle_kepfeltolto_kurzor.py
@@ -21,15 +21,47 @@ cursor.execute(f"""select * from haplmelinda_oldal_kep""")
 print("*"*200)
 for a in cursor:
     print("Values:", a)
+
+
 def technika(kep):
-    return 1
+    for i in [(1, 'fa'), (2, 'tűzzománc'), (3, 'üveg'), (4, 'Egyéb')]:
+        if kep[6] == i[1]:
+            return i[0]
+    return 4
+
+
 def tema(kep):
-    return 1
+    for i in [(1, 'női alak'), (2, 'női arc'), (3, 'természet'), (4, 'vallási téma'), (5, 'szerelem'), (6, 'virág'),
+     (7, 'mese figura'), (8, 'állat'), (9, 'mandala'), (10, 'Egyéb')]:
+        if kep[5] == i[1]:
+            return i[0]
+    return 10
 
 def eladva(kep):
     return 0 if kep[8] == 'false' else 1
 
-for i,kep in enumerate(kep_csv_adatok, 2):
+# temak = ["női alak","női arc","természet", "vallási téma", "szerelem", "virág", "mese figura", "állat", "mandala", "Egyéb"]
+# k = []
+# for i,temanev in enumerate(temak, 1):
+#     k.append((i,temanev))
+#     parancs = f"""
+#             INSERT INTO haplmelinda_oldal_tema VALUES({i}, '{temanev}')
+#             """
+#     print(parancs)
+#     cursor.execute(parancs)
+# print(k)
+#
+# technikak = ["fa","tűzzománc","üveg", "vallási téma", "szerelem", "virág", "mese figura", "állat", "mandala", "Egyéb"]
+# k = []
+# for i,technika in enumerate(technikak, 1):
+#     k.append((i,technika))
+#     parancs = f"""
+#             INSERT INTO haplmelinda_oldal_technika VALUES({i},'{technika}')
+#             """
+#     cursor.execute(parancs)
+# print(k)
+#
+for i,kep in enumerate(kep_csv_adatok, 1):
     if os.path.exists(f'haplmelinda_app/upload/{kep[0]}.jpg'):
         print(f"[{kep[0]}] Jó :)")
         parancs = f"""
